999bcafab0dbdd5862480e4b344a8cbd74a7819d
Polina Janochkina <janochkina.polina@gmail.com>
2018-06-02 17:36:37 +0700

    Views refactored

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,38 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Polina',
+        username: 'ursapolly',
+        avatar_url: 'https://img1.ak.crunchyroll.com/i/spire1/89bcf84b15a6cfbe05487ad5864504401488371587_large.png'
+      ),
+      User.new(
+        id: 2,
+        name: 'Irina',
+        username: 'irrr'
+      )
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = 'Hello!'
+    @user = User.new(
+      name: 'Polina',
+      username: 'ursapolly',
+      avatar_url: 'https://img1.ak.crunchyroll.com/i/spire1/89bcf84b15a6cfbe05487ad5864504401488371587_large.png'
+    )
+
+    @questions = [
+      Question.new(text: 'Как ты?', created_at: Date.parse('02.06.2018')),
+      Question.new(text: 'Любимое время года?', created_at: Date.parse('02.06.2018'))
+    ]
+
+    @new_question = Question.new
   end
 end
