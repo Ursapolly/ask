d2b14ef246f091e7c45b44c1c259289dc2365f44
Polina Janochkina <janochkina.polina@gmail.com>
2018-06-03 15:14:17 +0700

    Question_controller improved

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
     end
   end
 
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
+  end
+
   def declension(number, vopros, voprosa, voprosov)
     if number.nil? || !number.is_a?(Numeric)
       number = 0
--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,10 @@ module Askme
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.time_zone = 'Krasnoyarsk'
+    config.i18n.default_locale = :en
+    config.i18n.locale = :ru
+    config.i18n.fallbacks = [:en]
   end
 end
