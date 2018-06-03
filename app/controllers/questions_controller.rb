a06003767fd26e1789723ff04e47c104b55051ee
Polina Janochkina <janochkina.polina@gmail.com>
2018-06-03 15:25:28 +0700

    Security user_id fixed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
   before_action :authorize_user, except: [:create]
 
-  def edit
+  def edit;
   end
 
   def create
@@ -40,6 +40,10 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:user_id, :text, :answer)
+    if current_user.present? && params[:question][:user_id].to_i == current_user.id
+      params.require(:question).permit(:user_id, :text, :answer)
+    else
+      params.require(:question).permit(:user_id, :text)
+    end
   end
 end
