c62a7f3904423f9bda01a5d02caac16f924c3425
Polina Janochkina <janochkina.polina@gmail.com>
2018-06-03 07:39:52 +0700

    Questions counter added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
       asset_path 'avatar.jpeg'
     end
   end
+
+  def declension(number, vopros, voprosa, voprosov)
+    if number.nil? || !number.is_a?(Numeric)
+      number = 0
+    end
+
+    return voprosov if number >= 11 && number <= 14
+
+    remainder = number % 10
+    remainder2 = number % 100
+
+    return vopros if remainder == 1
+    return voprosa if remainder >= 2 && remainder <= 4
+    return voprosov if (remainder >= 5 && remainder <= 9) || remainder.zero?
+    return voprosov if remainder2 >= 11 && remainder2 <= 14
+  end
 end
