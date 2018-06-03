c956716395dbc072427f8779f7581e8b73485a0c
Polina Janochkina <janochkina.polina@gmail.com>
2018-06-03 13:26:31 +0700

    Sessions_controller created, autentication and athorization option available

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.authenticate(params[:email], params[:password])
+    if @user.present?
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'Добро пожаловать!'
+    else
+      flash.now.alert = 'Неправильные данные'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Вы вышли... Возвращайтесь скорее!'
+  end
+end
